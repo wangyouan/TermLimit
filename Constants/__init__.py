2294e78c975e78f859df62c2dd7401373feaa74a
Mark Wang <wangyouan0629@hotmail.com>
2020-03-06 15:31:47 +0800

    update constant and path info

--- Constants/__init__.py
+++ Constants/__init__.py
@@ -5,3 +5,14 @@
 # @Date: 2020/3/6
 # @Author: Mark Wang
 # @Email: wangyouan@gamil.com
+
+from .path_info import PathInfo
+
+
+class Constants(PathInfo):
+    GVKEY = 'gvkey'
+    COUNTRY = 'country'
+    YEAR = 'year'
+    CIK = 'cik'
+    CURRENCY = 'ioscur'
+    SIC = 'sic_code'
--- Constants/path_info.py
+++ Constants/path_info.py
@@ -5,3 +5,29 @@
 # @Date: 2020/3/6
 # @Author: Mark Wang
 # @Email: wangyouan@gamil.com
+
+import os
+
+
+class PathInfo(object):
+    ROOT_PATH = '/home/zigan/Documents/wangyouan/research/TermLimit'
+
+    DATA_PATH = os.path.join(ROOT_PATH, 'data')
+    TEMP_PATH = os.path.join(ROOT_PATH, 'temp')
+    RESULT_PATH = os.path.join(ROOT_PATH, 'output')
+    STATA_PATH = os.path.join(ROOT_PATH, 'stata')
+    STATA_CODE_PATH = os.path.join(STATA_PATH, 'code')
+    STATA_DATA_PATH = os.path.join(STATA_PATH, 'data')
+    STATA_RESULT_PATH = os.path.join(STATA_PATH, 'result')
+
+    DATABASE_PATH = '/home/zigan/Documents/wangyouan/database'
+
+    WRDS_PATH = '/home/zigan/Documents/wangyouan/database/wrds'
+    TFN_PATH = os.path.join(WRDS_PATH, 'tfn')
+    TFN_OWNER_PATH = os.path.join(TFN_PATH, 'ownership')
+    TFN_S34_PATH = os.path.join(TFN_PATH, 's34')
+
+    REGRESSION_PATH = '/home/zigan/Documents/wangyouan/research/DataMining/OLS'
+    REG_CONFIG_PATH = os.path.join(REGRESSION_PATH, 'config', 'TermLimits')
+    REG_RESULT_PATH = os.path.join(REGRESSION_PATH, 'result', 'TermLimits')
+    REG_DATA_PATH = os.path.join(REGRESSION_PATH, 'data', 'TermLimits')
