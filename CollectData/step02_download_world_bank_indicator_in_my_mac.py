a4fafedee33bf06bc8cd36c08f370abb95372e20
Mark Wang <wangyouan0629@hotmail.com>
2020-03-11 17:30:27 +0800

    update config file

--- CollectData/step02_download_world_bank_indicator_in_my_mac.py
+++ CollectData/step02_download_world_bank_indicator_in_my_mac.py
@@ -14,18 +14,29 @@ import os
 import time
 import random
 
+import pandas as pd
 from tqdm import tqdm
 import wbdata
 from pandas import DataFrame
 
 if __name__ == '__main__':
     source_list = wbdata.get_source(display=False)
-    indicator_index_df = DataFrame(columns=['SourceID', 'SourceName', 'IndicatorID', 'IndicatorName',
-                                            'sourceOrganization', 'sourceNote'])
+
     save_path = '/Users/markwang/Google Drive/Projects/DatabaseInformation/WorldBank'
+    save_file = os.path.join(save_path, '20200311_indicator_information.xlsx')
+
+    if not os.path.isfile(save_file):
+        indicator_index_df = DataFrame(columns=['SourceID', 'SourceName', 'IndicatorID', 'IndicatorName',
+                                                'sourceOrganization', 'sourceNote'])
+    else:
+        indicator_index_df: DataFrame = pd.read_excel(save_file)
+        indicator_index_df['SourceID'] = indicator_index_df['SourceID'].astype(int)
+
     for source_info in tqdm(source_list):
         source_id = source_info['id']
         info_dict = {'SourceID': source_id, 'SourceName': source_info['name']}
+        if not indicator_index_df.loc[indicator_index_df['SourceID'] == int(source_id)].empty:
+            continue
 
         indicator_list = wbdata.get_indicator(source=source_id, display=False)
         source_indicator_dfs = list()
@@ -39,6 +50,6 @@ if __name__ == '__main__':
                     indicator_info_dict[key] = indicator_info[key]
 
             indicator_index_df: DataFrame = indicator_index_df.append(indicator_info_dict, ignore_index=True)
+        indicator_index_df.drop_duplicates().to_excel(os.path.join(save_path, '20200311_indicator_information.xlsx'),
+                                                      index=False)
         time.sleep(random.randint(1, 10))
-
-    indicator_index_df.to_excel(os.path.join(save_path, '20200311_indicator_information.xlsx'), index=False)
