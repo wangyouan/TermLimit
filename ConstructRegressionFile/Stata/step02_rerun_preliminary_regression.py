54bb1e086f9496911d59202bb00acd1b97168f06
Mark Wang <wangyouan0629@hotmail.com>
2020-03-20 17:30:28 +0800

    add quotation mark

--- ConstructRegressionFile/Stata/step02_rerun_preliminary_regression.py
+++ ConstructRegressionFile/Stata/step02_rerun_preliminary_regression.py
@@ -49,7 +49,7 @@ def generate_regression_code(dep, ind, ctrl, fe_option, cluster_option, output_p
                              condition=''):
     return ['capture qui reghdfe {dep} {ind} {ctrl} {condition}, absorb({fe}) cl({cl})'.format(
         dep=dep, ind=ind, ctrl=ctrl, fe=fe_option, cl=cluster_option, condition=condition),
-        'outreg2 using {output_file}, addtext({output_text}) {dataconfig} nolabel append'.format(
+        'outreg2 using "{output_file}", addtext({output_text}) {dataconfig} nolabel append'.format(
             output_file=output_path, output_text=text_option,
             dataconfig=data_description), '']
 
