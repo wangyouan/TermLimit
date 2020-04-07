1d4824720d84ff164428a1a116b90120ddcad276
Mark Wang <wangyouan0629@hotmail.com>
2020-04-07 13:51:18 +0800

    update generate code

--- Utilities/generate_stata_code.py
+++ Utilities/generate_stata_code.py
@@ -11,7 +11,7 @@ def generate_foreach2_dep_ind_code(dep, ind, ctrl, fe_option, cluster_option, ou
                                    data_description, condition=''):
     return ['foreach dep_var in {}{{'.format(dep),
             'foreach ind_var in {}{{'.format(ind),
-            "capture qui reghdfe `dep_var' `ind_var' {ctrl} {condition}, absorb({fe}) cl({cl})".format(
+            "capture qui reghdfe `dep_var' `ind_var' {ctrl} {condition}, absorb({fe}) cl({cl}) keepsingle".format(
                 dep=dep, ctrl=ctrl, fe=fe_option, cl=cluster_option, condition=condition),
             'qui outreg2 using "{output_file}", addtext({output_text}) {dataconfig} nolabel append'.format(
                 output_file=output_path, output_text=text_option,
@@ -22,7 +22,7 @@ def generate_foreach2_dep_ind_code(dep, ind, ctrl, fe_option, cluster_option, ou
 def generate_foreach_dep_code(dep, ind, ctrl, fe_option, cluster_option, output_path, text_option,
                               data_description, condition=''):
     return ['foreach dep_var in {}{{'.format(dep),
-            "capture qui reghdfe `dep_var' {ind} {ctrl} {condition}, absorb({fe}) cl({cl})".format(
+            "capture qui reghdfe `dep_var' {ind} {ctrl} {condition}, absorb({fe}) cl({cl}) keepsingle".format(
                 dep=dep, ctrl=ctrl, fe=fe_option, cl=cluster_option, condition=condition, ind=ind),
             'qui outreg2 using "{output_file}", addtext({output_text}) {dataconfig} nolabel append'.format(
                 output_file=output_path, output_text=text_option,
@@ -32,7 +32,7 @@ def generate_foreach_dep_code(dep, ind, ctrl, fe_option, cluster_option, output_
 
 def generate_regression_code(dep, ind, ctrl, fe_option, cluster_option, output_path, text_option, data_description,
                              condition=''):
-    return ['capture qui reghdfe {dep} {ind} {ctrl} {condition}, absorb({fe}) cl({cl})'.format(
+    return ['capture qui reghdfe {dep} {ind} {ctrl} {condition}, absorb({fe}) cl({cl}) keepsingle'.format(
         dep=dep, ind=ind, ctrl=ctrl, fe=fe_option, cl=cluster_option, condition=condition),
         'outreg2 using "{output_file}", addtext({output_text}) {dataconfig} nolabel append'.format(
             output_file=output_path, output_text=text_option,
